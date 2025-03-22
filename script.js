5c89861186fd6c9f68e18362a9e3af5bb30a2955
KermaPol <nwmgow7yz3d@outlook.com>
2025-03-22 04:43:56 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-// Backup redirect if meta refresh fails
-setTimeout(() => {
-  if (window.location.href === document.location.href) {
-    window.location.href = 'https://ecostarmaroc.com/';
-  }
-}, 3500);
